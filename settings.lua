dc4002c3e762044d19d3e9dff7e9305443e1e06e
Andre Van Der Merwe <andre@andrevdm.com>
2026-06-07 06:56:42 +0100

    version bump for release

--- settings.lua
+++ settings.lua
@@ -4,7 +4,7 @@ data:extend({
     name = "nullius-early-transport-drones",
     setting_type = "startup",
     localised_name = "Early transport drones",
-    localised_description = "Move the initial transport drone system from the robotics era to the traffic control era, move depot infrastructure earlier, and replace early hangar-gated depot ingredients with train/circuit-era components.",
+    localised_description = "Move the transport drones from the robotics era to the traffic control era",
     default_value = false
   }
 })
